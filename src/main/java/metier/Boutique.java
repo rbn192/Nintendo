336f47bfa7ef991e9d911a29349d8044a40f4359
Chaymae <chaymae.sati@gmail.com>
2022-02-23 11:45:54 +0100

    class boutique

---
+++ src/main/java/metier/Boutique.java
@@ -0,0 +1,26 @@
+package metier;
+
+public class Boutique {
+	private String nom;
+	private String Adresse;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getAdresse() {
+		return Adresse;
+	}
+	public void setAdresse(String adresse) {
+		Adresse = adresse;
+	}
+	
+	@Override
+	public String toString() {
+		return "Boutique [nom=" + nom + ", Adresse=" + Adresse + "]";
+	}
+	
+
+}
