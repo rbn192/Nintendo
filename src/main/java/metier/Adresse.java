99723c762f2e996def5bc1c2a6786ab1cf610fb2
Emilie <emilie.v0613@gmail.com>
2022-02-23 11:45:50 +0100

    add class Adresse

---
+++ src/main/java/metier/Adresse.java
@@ -0,0 +1,48 @@
+package metier;
+
+public class Adresse {
+	
+	private String numero;
+	private String rue;
+	private String ville;
+	
+	public Adresse(String numero, String rue, String ville) {
+		this.numero = numero;
+		this.rue = rue;
+		this.ville = ville;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	@Override
+	public String toString() {
+		return "Adresse [numero=" + numero + ", rue=" + rue + ", ville=" + ville + "]";
+	}
+	
+	
+	
+	
+
+}
